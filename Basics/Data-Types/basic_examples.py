87114973b800bc0e65a849f414568809d1b6e335
scientist <scientistegong8@gmail.com>
2025-10-01 22:12:15 +0100

    updated codes for each folder

---
+++ Basics/Data-Types/basic_examples.py
@@ -0,0 +1,22 @@
+print("Hello Cybersecurity!")  # Cybersecurity
+
+# Naming Variables the right and wrong way.
+first_name = "scientist"
+last_name = "egong"
+print("\nMy name is {} {}.".format(first_name.title(), last_name.title())) # My name is Scientist Egong.
+
+
+# # Wrong variable naming conventions.
+# 1st_name = "Noah"
+# 2nd_name = "Albert"
+# print(1st_name, 2nd_name)
+
+# Assign values to a variable and print it.
+program_language = "python"
+print("\n{} is fun!".format(program_language.title()))  # Python is fun!
+
+
+# Reassign a new value to the existing variable and print output.
+occupation = "Fullstack Developer"  # Before
+occupation = "Cybersecurity Engineer"  # After
+print(occupation)
