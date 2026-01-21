5d6c1d3fa9d97613d21c1e2daee77274c733c50d
scientist <scientistegong8@gmail.com>
2026-01-21 10:20:11 +0100

    added atm.py project

--- Basics/conditionals/atm.py
+++ Basics/conditionals/atm.py
@@ -5,6 +5,35 @@
 user_balance = 0
 pin_trial = 5
 
+
+# -- Pin Verification --
+while pin_trial > 0:
+    try:
+        pin = int(input("Enter user pin: "))
+
+        # -- Correct pin --
+        if pin == (2520):
+            print("Pin verified successfully!")
+            break
+        
+        # -- Validate pin --
+        if (len(str(pin)) != 4 ):
+            print("Pin not complete! Try again.")
+            pin_trial -= 1
+            print(f"You have {pin_trial} trials remaining to input the correct pin.")
+
+
+    except (ValueError, TypeError):
+        print("Error in pin input!")
+
+
+# -- Check if user exceeded pin attempts --
+if pin_trial == 0:
+    print("No more trials remaining! Your account is temproary locked for the moment.\nExiting...")
+    exit()
+
+
+# -- ATM Operations --
 while True:
     print("--- ATM SIMULATOR ---")
     print("Enter the following options to proceed.\n" \
@@ -14,30 +43,20 @@ while True:
     "4. Exit\n")
     user_option = input("Enter an option to proceed: ")
 
+
+    # -- Exit option --
     if user_option.lower() in ["4", "quit", "q", "exit"]:
         print("Exiting...")
         break
 
-    while pin_trial > 0:
-        try:
-            pin = int(input("Enter user pin: "))
-            if pin == (2520):
-                break
-            
-            # -- Validate pin --
-            if (len(str(pin)) > 4)  or (len(str(pin)) < 4):
-                print("Pin not complete! Try again.")
-                pin_trial -= 1
-                print(f"You have {pin_trial} trials remaining to input the correct pin.")
+   
+    # -- Check Balance Option --
+    if user_option.lower().strip() in ["1", "check balance", "balance check"]:
+        print(f"User Balance: ${float(user_balance):.2f}")
+        print("\n")
 
-
-        except (ValueError, TypeError):
-            print("Error in pin input!")
-
-    if user_option.lower() in ["1", "check balance", "balance check"]:
-        print(f"User Balance: ${float(user_balance):.2f}/n")
-
-    elif user_option.lower() in ["2", "deposit"]:
+    # -- Deposit Option --
+    elif user_option.lower().strip() in ["2", "deposit"]:
         while True:
             try: 
                 deposit_amount = int(input("Enter amount to deposit: "))
@@ -51,8 +70,9 @@ while True:
                 break
 
             except ValueError:
-                print("Error in deposit input!")      
-    
+                print("Error in deposit input!")
+
+    # -- Withdraw Option --  
     elif user_option.lower() in ["3", "withdraw"]:
          while True:
             withdraw_input = input("Enter amount to withdraw or 'q' to quit: ")
@@ -61,14 +81,17 @@ while True:
 
             try: 
                 withdraw_amount = int(withdraw_input)
-                
+                if withdraw_amount < 0:
+                    print("Withdrawal amount must be positive!")
+                    continue
+
                 if withdraw_amount > user_balance:
                     print("Insufficient Funds! please fund your account.")
                     continue
 
             
                 user_balance -= float(withdraw_amount)
-                print(f"Account debit: ${withdraw_amount}!\nNew balance: ${user_balance:.2f}\n")
+                print(f"Account debit: ${withdraw_amount}\nNew balance: ${user_balance:.2f}\n")
                 break
                
                
